82d3bd105cfb156085c15558e5e0fed5cc8545e1
Dhruv Patel <dp221104@gmail.com>
2022-02-25 16:05:20 +0530

    DP: Added basic structure

---
+++ src/utils/Constant.js
@@ -0,0 +1,43 @@
+// export const baseUrl = "http://14.143.90.233:3000/"; //public
+// export const baseUrl = "http://192.168.0.218:3000/"; //internal
+export const baseUrl = "http://138.68.152.151:3000/"; //live
+import { Alert, Platform } from "react-native";
+
+export const opacity = 0.5;
+
+export const showMessage = (message) => {
+    if (Platform.OS == 'ios') {
+        Alert.alert(message, null);
+    } else {
+        Alert.alert(null, message);
+    }
+}
+
+export const showMessageWithCallBack = (message, callBack) => {
+    if (Platform.OS == 'ios') {
+        Alert.alert(message, null,
+            [
+                { text: 'OK', onPress: () => { callBack() } },
+            ]);
+    } else {
+        Alert.alert(null, message,
+            [
+                { text: 'OK', onPress: () => { callBack() } },
+            ]);
+    }
+}
+
+export const emailValidate = (text) => {
+    // console.log(text);
+    let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
+    if (reg.test(text) === false) {
+        console.log("Email is Not Correct");
+        //   this.setState({ email: text })
+        return false;
+    }
+    else {
+        //   this.setState({ email: text })
+        console.log("Email is Correct");
+        return true
+    }
+}
