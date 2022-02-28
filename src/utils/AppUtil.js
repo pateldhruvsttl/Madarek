391d738ba9b84231d50b63897aad8ef60e7f7ac8
sagarSTTL <sagar.khasiya@silvertouch.com>
2022-02-28 15:01:30 +0530

    Update AppUtil.js

--- src/utils/AppUtil.js
+++ src/utils/AppUtil.js
@@ -11,45 +11,11 @@ module.exports = {
 
     onLoding: (value) => {
         isLoding = value;
-
+        
         Loader.onLoding(value);
     },
 
-
-
-    showMessage: (message) => {
-        if (Platform.OS == 'ios') {
-            Alert.alert(message, null);
-        } else {
-            Alert.alert(null, message);
-        }
-    },
-
-    showMessageWithCallBack: (message, callBack) => {
-        if (Platform.OS == 'ios') {
-            Alert.alert(message, null,
-                [
-                    { text: 'OK', onPress: () => { callBack() } },
-                ]);
-        } else {
-            Alert.alert(null, message,
-                [
-                    { text: 'OK', onPress: () => { callBack() } },
-                ]);
-        }
-    },
-
-    emailValidate = (text) => {
-        let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
-        if (reg.test(text) === false) {
-            return false;
-        }
-        else {
-            return true
-        }
-    }
-
-}
+ }
 
 
 
