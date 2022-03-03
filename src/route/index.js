a75369013ef970dd96cabe5fc56b57e4bd12838e
Jalpa-SSTL <93371165+Jalpa-SSTL@users.noreply.github.com>
2022-03-03 17:46:45 +0530

    JB:Update index.js file

--- src/route/index.js
+++ src/route/index.js
@@ -7,6 +7,7 @@ import LoginScreen from '../screen/loginscreen/LoginScreen';
 import HomeSceen from '../screen/homescreen/HomeSceen';
 import Category from '../screen/category/Category';
 import Signup from '../screen/signup/Signup';
+import SignUpVerify from '../screen/signup/SignUpVerify';
 
 const Stack = createStackNavigator();
 
@@ -14,12 +15,13 @@ const Stack = createStackNavigator();
 export default function index() {
     return (
         <NavigationContainer>
-            <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName={"Signup"}>
+            <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName={"LoginScreen"}>
                 <Stack.Screen name="SplashScreen" component={SplashScreen} />
                 <Stack.Screen name="LoginScreen" component={LoginScreen} />
                 <Stack.Screen name="HomeSceen" component={HomeSceen} />
                 <Stack.Screen name="Category" component={Category} />
                 <Stack.Screen name="Signup" component={Signup} />
+                <Stack.Screen name="SignUpVerify" component={SignUpVerify} />
             </Stack.Navigator>
         </NavigationContainer>
     );
