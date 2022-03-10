48632c5f0c52e953db8084dcc62274d5e3b70c22
pradip-thummar-sttl <76721909+pradip-thummar-sttl@users.noreply.github.com>
2022-03-10 18:56:51 +0530

    drawer screen design complete

--- src/screen/drawer/MyDrawer.js
+++ src/screen/drawer/MyDrawer.js
@@ -14,37 +14,56 @@ import EnterpriseIcn from '../../assets/svg/drawerIcon/EnterpriseIcn'
 import HowItWorksIcn from '../../assets/svg/drawerIcon/HowItWorksIcn'
 import PartnerIcn from '../../assets/svg/drawerIcon/PartnerIcn'
 import SettingIcn from '../../assets/svg/drawerIcon/SettingIcn'
+import drawerStyles from './DrawerStyles'
 
 const MyDrawerScreen = (props) => {
   const { themeColor } = useSelector((state) => state)
+
+  const renderCollapseView = () => {
+    return (
+      <View>
+        {
+          ["Popular Ideas", "Latest Ideas", "Madarek Spotlight,", "Winning Ideas"].map((item, indes) => {
+            return (
+              <TouchableOpacity style={[drawerStyles.subMenuButton,{marginVertical: AppUtil.getHP(1),}]}>
+                <Text style={drawerStyles.menuText}>{item}</Text>
+              </TouchableOpacity>
+            )
+          })
+        }
+        <View style={drawerStyles.greyLine}></View>
+
+      </View>
+    )
+  }
   return (
     <View style={{ flex: 1 }}>
 
       {/* header */}
       <View style={{ height: AppUtil.getHP(23), backgroundColor: themeColor.headerColor, }}>
-        <View style={{ marginHorizontal: AppUtil.getWP(4), marginTop: AppUtil.getHP(4) }}>
+        <View style={drawerStyles.headerView}>
           <View style={drawerStyles.centerIcnView}>
             <IcnMenuHeader height={AppUtil.getHP(5)} width={AppUtil.getHP(20)} />
             <TouchableOpacity>
               <IcnClose height={AppUtil.getHP(2)} width={AppUtil.getHP(2)} />
             </TouchableOpacity>
           </View>
-          <View style={{ backgroundColor: themeColor.headerFontColor, width: '100%', height: 1, opacity: 0.5, marginTop: AppUtil.getHP(1) }} />
+          <View style={[drawerStyles.yellowLineView,{backgroundColor: themeColor.headerFontColor,}]} />
         </View>
 
-        <View style={{ flexDirection: "row", marginHorizontal: AppUtil.getWP(4), marginTop: AppUtil.getHP(2), alignItems: 'center' }}>
+        <View style={[drawerStyles.profileView]}>
           <Image style={drawerStyles.profileImage} />
-          <View style={{marginStart:5}}>
-            <Text style={{ fontFamily: FONTS.robotRegular, fontSize: AppUtil.getHP(2.05), color: GetAppColor.white }}>Welcome</Text>
-            <Text style={{ fontFamily: FONTS.robotMedium, fontSize: AppUtil.getHP(2.66), color: GetAppColor.white }}>Abimanyu Gayakwad</Text>
+          <View style={{ marginStart: 5 }}>
+            <Text style={drawerStyles.welcomeText}>Welcome</Text>
+            <Text style={drawerStyles.userNameText}>Abimanyu Gayakwad</Text>
           </View>
         </View>
-        <View style={{ flexDirection: 'row', marginHorizontal: AppUtil.getWP(16), marginTop: AppUtil.getHP(1), }}>
-          <TouchableOpacity style={{ borderColor: GetAppColor.white, borderWidth: 0.8, borderRadius: 5 }}>
-            <Text style={{ fontFamily: FONTS.robotRegular, fontSize: AppUtil.getHP(1.5), color: GetAppColor.white, marginHorizontal: AppUtil.getHP(1), marginVertical: AppUtil.getHP(0.5) }}>Dashboard</Text>
+        <View style={drawerStyles.dsButtonView}>
+          <TouchableOpacity style={drawerStyles.dashBoardButton}>
+            <Text style={drawerStyles.dashText}>Dashboard</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={{ borderColor: GetAppColor.white, borderWidth: 0.8, borderRadius: 5, marginStart: 5 }}>
-            <Text style={{ fontFamily: FONTS.robotRegular, fontSize: AppUtil.getHP(1.5), color: GetAppColor.white, marginHorizontal: AppUtil.getHP(1), marginVertical: AppUtil.getHP(0.5) }}>Submit Idea</Text>
+          <TouchableOpacity style={[drawerStyles.dashBoardButton,{  marginStart: 5 }]}>
+            <Text style={drawerStyles.dashText}>Submit Idea</Text>
           </TouchableOpacity>
         </View>
       </View>
@@ -52,65 +71,66 @@ const MyDrawerScreen = (props) => {
 
       {/*  */}
 
-      <TouchableOpacity style={{ marginHorizontal: AppUtil.getWP(4), height: AppUtil.getHP(6), borderBottomColor: GetAppColor.borderGray, borderBottomWidth: 1, marginTop: AppUtil.getHP(1), flexDirection: 'row', alignItems: 'center', }}>
+      <TouchableOpacity style={[drawerStyles.menuButton,{ marginTop: AppUtil.getHP(1)}]}>
         <HomeIcn height={AppUtil.getHP(3)} width={AppUtil.getHP(3)} />
-        <Text style={{ fontFamily: FONTS.robotRegular, fontSize: AppUtil.getHP(2.25), color: GetAppColor.pincolor, marginStart: AppUtil.getWP(2), marginTop: 5 }}>Home</Text>
+        <Text style={drawerStyles.menuText}>Home</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={{ marginHorizontal: AppUtil.getWP(4), height: AppUtil.getHP(6), borderBottomColor: GetAppColor.borderGray, borderBottomWidth: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+      <TouchableOpacity style={[drawerStyles.menuButton,{justifyContent: 'space-between' }]}>
         <View style={{ flexDirection: 'row' }}>
           <IdeaIcn height={AppUtil.getHP(3)} width={AppUtil.getHP(3)} />
-          <Text style={{ fontFamily: FONTS.robotRegular, fontSize: AppUtil.getHP(2.25), color: GetAppColor.pincolor, marginStart: AppUtil.getWP(2), marginTop: 5 }}>Ideas</Text>
+          <Text style={drawerStyles.menuText}>Ideas</Text>
         </View>
         <DownArrow height={AppUtil.getHP(2)} width={AppUtil.getHP(2)} />
       </TouchableOpacity>
 
       {/*  */}
+      {renderCollapseView()}
 
-      <TouchableOpacity style={{ marginHorizontal: AppUtil.getWP(4), height: AppUtil.getHP(6), borderBottomColor: GetAppColor.borderGray, borderBottomWidth: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+      <TouchableOpacity style={[drawerStyles.menuButton,{justifyContent: 'space-between' }]}>
         <View style={{ flexDirection: 'row' }}>
           <IdeaIcn height={AppUtil.getHP(3)} width={AppUtil.getHP(3)} />
-          <Text style={{ fontFamily: FONTS.robotRegular, fontSize: AppUtil.getHP(2.25), color: GetAppColor.pincolor, marginStart: AppUtil.getWP(2), marginTop: 5 }}>Challenges</Text>
+          <Text style={drawerStyles.menuText}>Challenges</Text>
         </View>
         <DownArrow height={AppUtil.getHP(2)} width={AppUtil.getHP(2)} />
       </TouchableOpacity>
 
 
-{/*  */}
-      <TouchableOpacity style={{ marginHorizontal: AppUtil.getWP(4), height: AppUtil.getHP(6), borderBottomColor: GetAppColor.borderGray, borderBottomWidth: 1, flexDirection: 'row', alignItems: 'center', }}>
+      {/*  */}
+      <TouchableOpacity style={drawerStyles.menuButton}>
         <UserIcn height={AppUtil.getHP(3)} width={AppUtil.getHP(3)} />
-        <Text style={{ fontFamily: FONTS.robotRegular, fontSize: AppUtil.getHP(2.25), color: GetAppColor.pincolor, marginStart: AppUtil.getWP(2), marginTop: 5 }}>Experts</Text>
+        <Text style={drawerStyles.menuText}>Experts</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={{ marginHorizontal: AppUtil.getWP(4), height: AppUtil.getHP(6), borderBottomColor: GetAppColor.borderGray, borderBottomWidth: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+      <TouchableOpacity style={[drawerStyles.menuButton,{justifyContent: 'space-between' }]}>
         <View style={{ flexDirection: 'row' }}>
           <EnterpriseIcn height={AppUtil.getHP(3)} width={AppUtil.getHP(3)} />
-          <Text style={{ fontFamily: FONTS.robotRegular, fontSize: AppUtil.getHP(2.25), color: GetAppColor.pincolor, marginStart: AppUtil.getWP(2), marginTop: 5 }}>Enterprises</Text>
+          <Text style={drawerStyles.menuText}>Enterprises</Text>
         </View>
         <DownArrow height={AppUtil.getHP(2)} width={AppUtil.getHP(2)} />
       </TouchableOpacity>
 
       {/*  */}
 
-      <TouchableOpacity style={{ marginHorizontal: AppUtil.getWP(4), height: AppUtil.getHP(6), borderBottomColor: GetAppColor.borderGray, borderBottomWidth: 1, flexDirection: 'row', alignItems: 'center', }}>
+      <TouchableOpacity style={drawerStyles.menuButton}>
         <HowItWorksIcn height={AppUtil.getHP(3)} width={AppUtil.getHP(3)} />
-        <Text style={{ fontFamily: FONTS.robotRegular, fontSize: AppUtil.getHP(2.25), color: GetAppColor.pincolor, marginStart: AppUtil.getWP(2), marginTop: 5 }}>How it Works</Text>
+        <Text style={drawerStyles.menuText}>How it Works</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={{ marginHorizontal: AppUtil.getWP(4), height: AppUtil.getHP(6), borderBottomColor: GetAppColor.borderGray, borderBottomWidth: 1, flexDirection: 'row', alignItems: 'center', }}>
+      <TouchableOpacity style={drawerStyles.menuButton}>
         <PartnerIcn height={AppUtil.getHP(3)} width={AppUtil.getHP(3)} />
-        <Text style={{ fontFamily: FONTS.robotRegular, fontSize: AppUtil.getHP(2.25), color: GetAppColor.pincolor, marginStart: AppUtil.getWP(2), marginTop: 5 }}>Partners</Text>
+        <Text style={drawerStyles.menuText}>Partners</Text>
       </TouchableOpacity>
 
-      <View style={{ position: 'absolute', bottom: 0, height: AppUtil.getHP(6), backgroundColor: GetAppColor.backGround, width: '100%', flexDirection:'row' }}>
-        <TouchableOpacity style={{ width: '50%', height: '100%', alignItems: 'center', justifyContent: 'center', flexDirection:'row'}}>
+      <View style={drawerStyles.bottomView}>
+        <TouchableOpacity style={drawerStyles.settingButton}>
           <SettingIcn height={AppUtil.getHP(2.5)} width={AppUtil.getHP(2.5)} />
-          <Text style={{ fontFamily: FONTS.robotRegular, fontSize: AppUtil.getHP(2.05), color: GetAppColor.pincolor, marginStart: AppUtil.getWP(2),}}>Settings</Text>
+          <Text style={drawerStyles.settingText}>Settings</Text>
         </TouchableOpacity>
-        <View style={{ width: 1, backgroundColor: GetAppColor.borderGray, height: '60%', alignSelf:'center' }} />
-        <TouchableOpacity style={{ width: '50%', height: '100%', alignItems: 'center', justifyContent: 'center', flexDirection:'row'}}>
+        <View style={drawerStyles.line} />
+        <TouchableOpacity style={drawerStyles.settingButton}>
           <SettingIcn height={AppUtil.getHP(2.5)} width={AppUtil.getHP(2.5)} />
-          <Text style={{ fontFamily: FONTS.robotRegular, fontSize: AppUtil.getHP(2.05), color: GetAppColor.pincolor, marginStart: AppUtil.getWP(2),}}>Logout</Text>
+          <Text style={drawerStyles.settingText}>Logout</Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -120,8 +140,4 @@ const MyDrawerScreen = (props) => {
 export default MyDrawerScreen;
 
 
-const drawerStyles = StyleSheet.create({
-  centerIcnView: { width: '100%', alignItems: 'flex-start', flexDirection: 'row', justifyContent: 'space-between', },
-  profileImage: { height: AppUtil.getHP(5), width: AppUtil.getHP(5), borderRadius: AppUtil.getHP(5 / 2), borderWidth: 1, borderColor: 'white' },
 
-})
