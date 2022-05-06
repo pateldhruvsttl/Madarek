11e465dbba9d8c774f320c5c388aea2e22bd4c53
Jalpa-SSTL <93371165+Jalpa-SSTL@users.noreply.github.com>
2022-05-06 10:20:00 +0530

    JB : Update some Change

--- src/screen/livechat/LiveChatStyle.js
+++ src/screen/livechat/LiveChatStyle.js
@@ -6,10 +6,10 @@ import FONTS from '../../utils/Fonts'
 const STYLE = StyleSheet.create({
     container: {
         backgroundColor: GetAppColor.white,
-        justifyContent: 'center', 
+        justifyContent: 'center',
         flex: 1
     },
-    listing : {
+    listing: {
         width: '100%',
         marginBottom: AppUtil.getHP(10)
     },
@@ -18,64 +18,70 @@ const STYLE = StyleSheet.create({
         height: AppUtil.getHP(10),
         position: 'absolute',
         bottom: 0,
-        flexDirection:'row',
-        alignItems:'center',
+        flexDirection: 'row',
+        alignItems: 'center',
     },
-    labelContainer : {
-        flexDirection:'row',
-        alignItems:'center',
+    labelContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
         width: AppUtil.getWP(55),
         borderRadius: AppUtil.getHP(3),
         backgroundColor: GetAppColor.chatInputColor,
-        height:AppUtil.getHP(5.5),
+        height: AppUtil.getHP(5.5),
         paddingStart: AppUtil.getWP(5),
         marginStart: AppUtil.getWP(4),
-        justifyContent:'space-between'
+        justifyContent: 'space-between'
     },
     chatInput: {
         color: GetAppColor.pincolor,
-        fontSize : AppUtil.getHP(1.8),
-        width : AppUtil.getWP(40),
+        fontSize: AppUtil.getHP(1.8),
+        width: AppUtil.getWP(40),
     },
-    icnContainer : { marginStart: AppUtil.getWP(4),flexDirection:'row',alignItems:'center',justifyContent:'center'},
-    icons : {
-       marginEnd:AppUtil.getWP(4)
+    icnContainer: {
+        marginStart: AppUtil.getWP(4),
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center'
     },
-    menuDocument : {
-        marginEnd:AppUtil.getWP(2)
+    icons: {
+        marginEnd: AppUtil.getWP(4)
     },
-    leftMsg : {
+    menuDocument: {
+        marginEnd: AppUtil.getWP(2)
+    },
+    leftMsg: {
         backgroundColor: GetAppColor.chatInputColor,
-        width:AppUtil.getWP(50),
+        width: AppUtil.getWP(50),
         borderBottomRightRadius: AppUtil.getHP(2),
-        borderTopLeftRadius:AppUtil.getHP(2),
-        borderTopRightRadius:AppUtil.getHP(2),
+        borderTopLeftRadius: AppUtil.getHP(2),
+        borderTopRightRadius: AppUtil.getHP(2),
         padding: AppUtil.getHP(1.5),
-        
+
     },
-    rightMsg : {
+    rightMsg: {
         backgroundColor: GetAppColor.chatInputColor,
-        width:AppUtil.getWP(50),
-        borderTopLeftRadius:AppUtil.getHP(2),
-        borderTopRightRadius:AppUtil.getHP(2),
-        borderBottomLeftRadius:AppUtil.getHP(2),
+        width: AppUtil.getWP(50),
+        borderTopLeftRadius: AppUtil.getHP(2),
+        borderTopRightRadius: AppUtil.getHP(2),
+        borderBottomLeftRadius: AppUtil.getHP(2),
         padding: AppUtil.getHP(1.5),
-        paddingStart : AppUtil.getHP(2.5),
+        paddingStart: AppUtil.getHP(2.5),
     },
     messageContainer: {
-        margin:20,
-        flex:1,
+        // margin: 20,
+        marginHorizontal :  AppUtil.getHP(1.5), 
+        flex: 1,
     },
-    leftLabel : {
-        color : GetAppColor.pincolor,
-        fontFamily:FONTS.robotRegular,
-        fontSize : AppUtil.getHP(1.8)
+    leftLabel: {
+        color: GetAppColor.pincolor,
+        fontFamily: FONTS.robotRegular,
+        fontSize: AppUtil.getHP(1.8)
     },
-    time : {
-        fontFamily:FONTS.robotRegular,
-        fontSize : AppUtil.getHP(1.6),
-        color:GetAppColor.timeTxtColor,
-        paddingTop:AppUtil.getHP(0.5)
+    time: {
+        fontFamily: FONTS.robotRegular,
+        fontSize: AppUtil.getHP(1.6),
+        color: GetAppColor.timeTxtColor,
+        paddingTop: AppUtil.getHP(0.5)
     }
 })
 
