5b70b419be49c577bcaf6b8790630819628565a1
sagarSTTL <sagar.khasiya@silvertouch.com>
2022-08-05 14:58:19 +0530

    make apk

--- src/component/submitideas/SubmitIdeaStep2.js
+++ src/component/submitideas/SubmitIdeaStep2.js
@@ -1,120 +1,75 @@
-import { View, Text, TouchableOpacity, TextInput, Alert } from 'react-native'
-import React, { memo, useState } from 'react'
+import { View, Text, TouchableOpacity, TextInput, Image } from 'react-native'
+import React, { memo, useEffect, useState } from 'react'
+
+import CountryPicker from 'react-native-country-picker-modal'
+import CustomList from './CustomList'
+
 import { Label } from '../../utils/StringUtil'
 import Style from './IdeaStepStyle'
 import { useSelector } from 'react-redux'
 import { GetAppColor } from '../../utils/Colors'
-import { AppUtil } from '../../utils/AppUtil'
-
+import BackIcon from '../../assets/svg/loginLogo/BackIcon'
+import ImageList from './ImageList';
+import { deviceId } from '../../utils/Constant'
+import { AppConfig } from '../../manager/AppConfig'
+import { UserManager } from '../../manager/UserManager'
 
 function SubmitIdeaStep2(props) {
 
     const { themeColor } = useSelector((state) => state);
+
+    const [countryCode, setCountryCode] = useState('IN');
+    const [mobileNumber, setMobileNumber] = useState("");
+    const [nationality, setNationality] = useState("Selected");
+    const [message, setMessage] = useState('');
+    const [imageList, setImageList] = useState([{ assets: "selected" }]);
     
-    const [selectedIndex, setSelectedIndex] = useState(5);
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [email, setEmail] = useState("");
+
+    useEffect(()=>{
+        
+        onGetField();
+    },[])
+
+    const onGetField =()=>{
+
+        let obj = {
+            "device_id":deviceId,
+            "token" :AppConfig.token,
+            "lang" :AppConfig.lang,
+             "frontuser_id" :UserManager.userId,
+             "idea_title":"test",
+             "sector_id":"2",
+             "category_id":"5",
+             "sub_category_id":"0"
+        }
+    }
 
     const onCheckField = () => {
-        // let gender = "";
-        // if(selectedIndex == 0) gender = "Male";
-        // else if(selectedIndex == 1) gender = "Female";    
-        // else if(selectedIndex == 2) gender = "Other";    
-
-        // var obj = { firstName: firstName, lastName: lastName, gender: gender, email: email }
-
-        // if(email === "" || !AppUtil.validate(email))
-        //     Alert.alert(Label.enteremail)
-        // else if(firstName === "" || lastName === "" || gender === 5 || email === "")
-        //     Alert.alert(Label.FillMandatoryFieldsValidation);
-        // else     
-            props.onNext(/*obj*/);
-
-        
+        // var obj ={countryCode:countryCode, mobileNumber:mobileNumber, nationality:nationality,message:message, imageList:imageList}
+        props.onNext(/*obj*/);
     }
+    const onSelect = (country) => {
+        setCountryCode(country.cca2)
+    }
+
     return (
         <View style={Style.MainView}>
 
-            <View style={Style.innerFirstView}>
-                <Text style={Style.txtTitle}>{Label.firstname}<Text style={Style.txtStar}>*</Text></Text>
+            <View style={Style.innerSecondView1}>
+                <Text style={Style.txtTitle}>{Label.Message}</Text>
                 <TextInput
                     placeholderTextColor={GetAppColor.grayBorder}
                     numberOfLines={2}
                     multiline={true}
                     style={Style.txtInputTitle}
-                    value={firstName}
-                    onChangeText={title => setFirstName(title)}
+                    value={message}
+                    onChangeText={title => setMessage(title)}
                 />
             </View>
 
-            <View style={Style.innerFirstView}>
-                <Text style={Style.txtTitle}>{Label.lastname}<Text style={Style.txtStar}>*</Text></Text>
-                <TextInput
-                    placeholderTextColor={GetAppColor.grayBorder}
-                    numberOfLines={2}
-                    multiline={true}
-                    style={Style.txtInputTitle}
-                    value={lastName}
-                    onChangeText={title => setLastName(title)}
-                />
-            </View>
-
-            <View style={Style.innerFirstView}>
-                <Text style={Style.txtTitle}>{Label.Gender}<Text style={Style.txtStar}>*</Text></Text>
-
-                <View style={Style.selectGender}>
-
-                    <TouchableOpacity onPress={() => setSelectedIndex(0)} style={[Style.tcOptions,{marginStart:0}]}>
-                        {
-                            selectedIndex == 0 ?
-                                <View style={Style.yellowBorderView}>
-                                    <View style={Style.yellowFillView} />
-                                </View>
-                                :
-                                <View style={Style.grayBorderView} />
-                        }
-                        <Text style={Style.txtOptions}>{Label.Male}</Text>
-
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => setSelectedIndex(1)} style={Style.tcOptions}>
-                        {
-                            selectedIndex == 1 ?
-                                <View style={Style.yellowBorderView}>
-                                    <View style={Style.yellowFillView} />
-                                </View>
-                                :
-                                <View style={Style.grayBorderView} />
-                        }
-                        <Text style={Style.txtOptions}>{Label.Female}</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => setSelectedIndex(2)} style={Style.tcOptions}>
-                        {
-                            selectedIndex == 2 ?
-                                <View style={Style.yellowBorderView}>
-                                    <View style={Style.yellowFillView} />
-                                </View>
-                                :
-                                <View style={Style.grayBorderView} />
-                        }
-                        <Text style={Style.txtOptions}>{Label.Other}</Text>
-                    </TouchableOpacity>
-
-                </View>
-                
-            </View>
-
-            <View style={Style.innerFirstView}>
-                <Text style={Style.txtTitle}>{Label.emailID}<Text style={Style.txtStar}>*</Text></Text>
-                <TextInput
-                    placeholderTextColor={GetAppColor.grayBorder}
-                    numberOfLines={2}
-                    multiline={true}
-                    style={Style.txtInputTitle}
-                    value={email}
-                    onChangeText={title => setEmail(title)}
-                />
-
+            <View style={Style.innerView1}>
+                <Text style={Style.txtTitle}>{Label.UploadedImages}</Text>
+                <ImageList onUpdateList={(list)=> setImageList(list)}/>
             </View>
 
             <TouchableOpacity style={[Style.btn, { backgroundColor: themeColor.buttonColor }]} onPress={() => onCheckField()}>
@@ -125,3 +80,5 @@ function SubmitIdeaStep2(props) {
 }
 
 export default memo(SubmitIdeaStep2);
+
+const sectorsList = ["Indian", "Indian", "Indian", "Indian", "Indian", "Indian", "Indian"];
--- src/component/submitideas/SubmitIdeaStep3.js
+++ src/component/submitideas/SubmitIdeaStep3.js
@@ -1,30 +1,36 @@
-import { View, Text, TouchableOpacity, TextInput, Image } from 'react-native'
+import { View, Text, TouchableOpacity, TextInput } from 'react-native'
 import React, { memo, useState } from 'react'
-
 import CountryPicker from 'react-native-country-picker-modal'
+import ImageList from './ImageList';
 import CustomList from './CustomList'
-
 import { Label } from '../../utils/StringUtil'
 import Style from './IdeaStepStyle'
-import { useSelector } from 'react-redux'
+import { useSelector} from 'react-redux'
 import { GetAppColor } from '../../utils/Colors'
 import BackIcon from '../../assets/svg/loginLogo/BackIcon'
-import ImageList from './ImageList';
+
 
 function SubmitIdeaStep3(props) {
-
     const { themeColor } = useSelector((state) => state);
+    const [title, setTitle] = useState(props?.step1Obj?.title);
+    const [sectors, setSectors] = useState(props?.step1Obj?.sectors);
+    const [category, setCategory] = useState(props?.step1Obj?.category);
+    const [subCategory, setSubCategory] = useState(props?.step1Obj?.subCategory);
 
-    const [countryCode, setCountryCode] = useState('IN');
-    const [mobileNumber, setMobileNumber] = useState("");
-    const [nationality, setNationality] = useState("Selected");
-    const [message, setMessage] = useState('');
-    const [imageList, setImageList] = useState([{ assets: "selected" }]);
+    const [firstName, setFirstName] = useState(props?.step2Obj?.firstName);
+    const [lastName, setLastName] = useState(props?.step2Obj?.lastName);
+    const [gender, setGender] = useState(props?.step2Obj?.gender);
+    const [emailId, setEmailId] = useState(props?.step2Obj?.email);
     
+    const [nationality, setNationality] = useState(props?.step3Obj?.nationality);
+    const [countryCode, setCountryCode] = useState(props?.step3Obj?.countryCode);
+    const [mobileNumber, setMobileNumber] = useState(props?.step3Obj?.mobileNumber);
+    const [message, setMessage] = useState(props?.step3Obj?.message);
+    const [imageList, setImageList] = useState(props?.step3Obj?.imageList);
 
+    
     const onCheckField = () => {
-        // var obj ={countryCode:countryCode, mobileNumber:mobileNumber, nationality:nationality,message:message, imageList:imageList}
-        props.onNext(/*obj*/);
+        props.onNext();
     }
     const onSelect = (country) => {
         setCountryCode(country.cca2)
@@ -34,9 +40,99 @@ function SubmitIdeaStep3(props) {
         <View style={Style.MainView}>
 
             <View style={Style.innerFirstView}>
+                <Text style={Style.txtTitle}>{Label.IdeaTitle}</Text>
+                <TextInput
+                    placeholderTextColor={GetAppColor.grayBorder}
+                    numberOfLines={2}
+                    multiline={true}
+                    style={Style.txtIdeasTitle}
+                    value={title}
+                    onChangeText={title => setTitle(title)}
+                />
+            </View>
+
+            <View style={Style.innerView2}>
+                <Text style={Style.txtTitle}>{Label.Sectors}</Text>
+                <CustomList currentItem={sectors} item={sectorsList} onSelect={(txt) => setSectors(txt)} isType={"Submit"} />
+            </View>
+
+            <View style={Style.innerView2}>
+                <Text style={Style.txtTitle}>{Label.Category}</Text>
+                <CustomList currentItem={category} item={sectorsList} onSelect={(txt) => setCategory(txt)} isType={"Submit"} />
+            </View>
+
+            <View style={Style.innerView2}>
+                <Text style={Style.txtTitle}>{Label.SubCategory}</Text>
+                <CustomList currentItem={subCategory} item={sectorsList} onSelect={(txt) => setSubCategory(txt)} isType={"Submit"} />
+            </View>
+
+            <View style={Style.txtNameView}>
+                <View style={Style.txtName}>
+                    <Text style={Style.txtTitle}>{Label.firstname}</Text>
+                    <TextInput
+                        placeholderTextColor={GetAppColor.grayBorder}
+                        numberOfLines={2}
+                        multiline={true}
+                        style={Style.txtIdeasTitle}
+                        value={firstName}
+                        onChangeText={title => setFirstName(title)}
+                    />
+                </View>
+                <View style={Style.txtName}>
+                    <Text style={Style.txtTitle}>{Label.lastname}</Text>
+                    <TextInput
+                        placeholderTextColor={GetAppColor.grayBorder}
+                        numberOfLines={2}
+                        multiline={true}
+                        style={Style.txtIdeasTitle}
+                        value={lastName}
+                        onChangeText={title => setLastName(title)}
+                    />
+                </View>
+            </View>
+
+            <View style={Style.txtNameView}>
+                <View style={Style.txtName}>
+                    <Text style={Style.txtTitle}>{Label.Gender}</Text>
+                    <TextInput
+                        placeholderTextColor={GetAppColor.grayBorder}
+                        numberOfLines={2}
+                        multiline={true}
+                        style={Style.txtIdeasTitle}
+                        value={gender}
+                        onChangeText={title => setGender(title)}
+                    />
+                </View>
+                <View style={Style.txtName}>
+                    <Text style={Style.txtTitle}>{Label.Nationality}</Text>
+                    <TextInput
+                        placeholderTextColor={GetAppColor.grayBorder}
+                        numberOfLines={2}
+                        multiline={true}
+                        style={Style.txtIdeasTitle}
+                        value={nationality}
+                        onChangeText={title => setNationality(title)}
+                    />
+                </View>
+            </View>
+
+            <View style={Style.innerSecondView2}>
+                <Text style={Style.txtTitle}>{Label.emailID}</Text>
+                <TextInput
+                    placeholderTextColor={GetAppColor.grayBorder}
+                    numberOfLines={2}
+                    multiline={true}
+                    style={Style.txtIdeasTitle}
+                    value={emailId}
+                    onChangeText={title => setEmailId(title)}
+                />
+            </View>
+
+            <View style={Style.innerSecondView2}>
                 <Text style={Style.txtTitle}>{Label.AlternateMobileNo}</Text>
-                <View style={Style.mobileInnerView}>
-                    <View style={Style.numberAreaOne1}>
+                <View style={{ flexDirection: 'row' }}>
+
+                    <View style={Style.numberAreaOne2}>
                         <CountryPicker
                             {...{ countryCode, onSelect, withCallingCode: true, withFlagButton: false, withCallingCodeButton: true, }}
                             visible={false}
@@ -53,37 +149,32 @@ function SubmitIdeaStep3(props) {
                         placeholderTextColor={GetAppColor.grayBorder}
                         numberOfLines={2}
                         keyboardType="number-pad"
-                        style={Style.txtInputMobile}
+                        style={Style.txtInputMobile2}
                         value={mobileNumber}
                         onChangeText={title => setMobileNumber(title)}
                     />
                 </View>
             </View>
 
-            <View style={Style.innerView1}>
-                <Text style={Style.txtTitle}>{Label.Nationality}</Text>
-                <CustomList currentItem={nationality} item={sectorsList} onSelect={(txt) => setNationality(txt)} />
-            </View>
-
-            <View style={Style.innerSecondView1}>
+            <View style={Style.innerSecondView2}>
                 <Text style={Style.txtTitle}>{Label.Message}</Text>
                 <TextInput
                     placeholderTextColor={GetAppColor.grayBorder}
                     numberOfLines={2}
                     multiline={true}
-                    style={Style.txtInputTitle}
+                    style={Style.txtIdeasTitle}
                     value={message}
                     onChangeText={title => setMessage(title)}
                 />
             </View>
 
-            <View style={Style.innerView1}>
+            <View style={Style.innerView2}>
                 <Text style={Style.txtTitle}>{Label.UploadedImages}</Text>
-                <ImageList onUpdateList={(list)=> setImageList(list)}/>
+                <ImageList onUpdateList={(list)=> setImageList(list)} currentList={imageList}/>
             </View>
 
             <TouchableOpacity style={[Style.btn, { backgroundColor: themeColor.buttonColor }]} onPress={() => onCheckField()}>
-                <Text style={Style.txtBtn}>{Label.Continue}</Text>
+                <Text style={Style.txtBtn}>{Label.Submit}</Text>
             </TouchableOpacity>
         </View>
     )
@@ -91,4 +182,4 @@ function SubmitIdeaStep3(props) {
 
 export default memo(SubmitIdeaStep3);
 
-const sectorsList = ["Indian", "Indian", "Indian", "Indian", "Indian", "Indian", "Indian"];
+const sectorsList = ["Slider", "Tab", "Challenges", "Spotlight", "ExpertInsightsSlider", "FavouriteCategories", "Button"];
