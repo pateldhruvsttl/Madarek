154326138a19a50d3eb0f5ba2badf673279d4cfd
sagarSTTL <sagar.khasiya@silvertouch.com>
2022-09-28 16:26:44 +0530

    Update SubmitIdeaStep2.js
    Make dynamik form data and it's request paramiters

--- src/component/submitideas/SubmitIdeaStep2.js
+++ src/component/submitideas/SubmitIdeaStep2.js
@@ -23,13 +23,18 @@ function SubmitIdeaStep2(props) {
     const [isMultiImage, setMultiImage] = useState([]);
     const [isFile, setFile] = useState({});
     const [isArror, setArror] = useState(false);
-    
+
+    const [isFormData, setFormData] = useState({});
+
+    useEffect(() => {
+        Loger.onLog("FormData", isFormData)
+    }, [isFormData])
 
     useEffect(() => {
         GetField(props?.data?.sectorsId, props?.data?.categoryId, props?.data?.subCategoryId);
     }, [])
 
- 
+
 
     const GetField = (sectorsId, categoryId, subCategoryId) => {
         let obj = {
@@ -61,11 +66,18 @@ function SubmitIdeaStep2(props) {
         );
     }
 
-    const onTextChange = (index, txt) => {
+    const onTextChange = (index, KeyType, txt) => {
 
         let arrData = isItemList;
         arrData[index].dataItem = txt;
         setItemList(arrData)
+
+        let _obj = {};
+        _obj = isFormData;
+        _obj[KeyType] = txt
+
+        Loger.onLog("onTextChange", _obj)
+        setFormData(_obj);
     }
 
     const onSelectFile = (index, KeyType, item) => {
@@ -73,41 +85,71 @@ function SubmitIdeaStep2(props) {
         let arrData = isItemList;
         arrData[index].dataItem = item;
         setItemList(arrData)
-        
-        let _obj ={};
+
+        let _obj = {};
         _obj = isFile;
         _obj[KeyType] = item
         setFile(_obj);
 
+        let data = {};
+        data = isFormData;
+        data[KeyType] = ""
+
+        setFormData(data);
+
     }
 
-    const onSelectMultiImg = (index, item) => {
+    const onSelectMultiImg = (index, KeyType, item) => {
         let arrData = isItemList;
         arrData[index].dataItem = item;
         setItemList(arrData)
         setMultiImage(item);
+
+        let _obj = {};
+        _obj = isFormData;
+        _obj[KeyType] = "";
+
+        setFormData(_obj);
+
     }
 
-    const onTeamMemeber = (index, item) => {
+    const onTeamMemeber = (index, KeyType, item) => {
 
         let arrData = isItemList;
         if (item !== "err") {
             arrData[index].dataItem = item;
             setItemList(arrData)
             setArror(false)
+
+
+            let _obj = {};
+            _obj = isFormData;
+            _obj[KeyType] = item
+            setFormData(_obj);
+
         }
         else {
             arrData[index].dataItem = "";
             setItemList(arrData)
             setArror(true)
+
+            let _obj = {};
+            _obj = isFormData;
+            _obj[KeyType] = {};
+            setFormData(_obj);
         }
 
     }
 
-    const onTermConditions = (index, item) => {
+    const onTermConditions = (index, KeyType, item) => {
         let arrData = isItemList;
         arrData[index].dataItem = item;
         setItemList(arrData)
+
+        let _obj = {};
+        _obj = isFormData;
+        _obj[KeyType] = item
+        setFormData(_obj);
     }
 
     const onCheckedAllRequredFiles = () => {
@@ -120,6 +162,7 @@ function SubmitIdeaStep2(props) {
         return validationFali;
 
     }
+
     const onCheckField = () => {
 
         if (!onCheckedAllRequredFiles())
@@ -129,94 +172,15 @@ function SubmitIdeaStep2(props) {
             return false;
         }
         else {
+
+            Loger.onLog("isFormData", isFormData);
+
             props.onNext({
-                data_obj: JSON.stringify(isItemList),
+                data_obj: JSON.stringify(isFormData),
                 obj: isFile,
                 isMultiImage: isMultiImage,
             });
         }
-
-
-
-        // if (isData?.idea_description?.required === "Y" && isIdeaDescription === "") {
-        //     Alert.alert(Label.FillMandatoryFieldsValidation);
-        // }
-        // else if (isData?.idea_cover_image?.required === "Y" && isIdeaCoverImage === "") {
-        //     Alert.alert(Label.FillMandatoryFieldsValidation);
-        // }
-        // else if (isData?.upload_additional_images?.required === "Y" && isMultiImage.length == 0) {
-        //     Alert.alert(Label.FillMandatoryFieldsValidation);
-        // }
-        // else if (isData?.challenges_addressing?.required === "Y" && isChallengesAddressing === "") {
-        //     Alert.alert(Label.FillMandatoryFieldsValidation);
-        // }
-        // else if (isData?.benefit_idea?.required === "Y" && isBenefitIdea === "") {
-        //     Alert.alert(Label.FillMandatoryFieldsValidation);
-        // }
-        // else if (isData?.idea_upload_files?.required === "Y" && isFile === "") {
-        //     Alert.alert(Label.FillMandatoryFieldsValidation);
-        // }
-        // else if (isData?.upload_video_url?.required === "Y" && isVideoUrl === "") {
-        //     Alert.alert(Label.FillMandatoryFieldsValidation);
-        // }
-        // else if (isData?.upload_embed_url?.required === "Y" && isUploadEmbedUrl === "") {
-        //     Alert.alert(Label.FillMandatoryFieldsValidation);
-        // }
-        // else if (isData?.idea_upload_videos?.required === "Y" && isVideoFile === "") {
-        //     Alert.alert(Label.FillMandatoryFieldsValidation);
-        // }
-        // else if (isData?.keywords_tags?.required === "Y" && iskeywordsTags === "") {
-        //     Alert.alert(Label.FillMandatoryFieldsValidation);
-        // }
-        // else if (isData?.no_of_team_member?.required === "Y" && isNoOfTeamMember === "") {
-        //     Alert.alert(Label.FillMandatoryFieldsValidation);
-        // }
-        // else if ((isData?.team_member_details?.required === "Y" && isTeamMember === "") || isArror == true) {
-        //     Alert.alert(Label.FillMandatoryFieldsValidation);
-        // }
-        // else if (isData?.hide_team_identity?.required === "Y" && isTeamIdentity === 0) {
-        //     Alert.alert(Label.FillMandatoryFieldsValidation);
-        // }
-        // else if (isData?.post_idea_privately?.required === "Y" && isIdeaPrivately === 0) {
-        //     Alert.alert(Label.FillMandatoryFieldsValidation);
-        // }
-        // else if (isData?.terms_condiitons?.required === "Y" && isTermsConditons === 0) {
-        //     Alert.alert(Label.FillMandatoryFieldsValidation);
-        // }
-        // else {
-
-        // let data_form = {
-        //     photos_image: null,
-        //     idea_cover_image: "",
-        //     idea_upload_files: "",
-        //     idea_upload_videos: "",
-        //     upload_additional_images: "",
-
-        //     idea_description: isIdeaDescription,
-        //     challenges_addressing: isChallengesAddressing,
-        //     benefit_idea: isBenefitIdea,
-        //     upload_video_url: isVideoUrl,
-        //     upload_embed_url: isUploadEmbedUrl,
-        //     keywords_tags: iskeywordsTags,
-        //     no_of_team_member: isNoOfTeamMember,
-        //     team_member_details: isTeamMember,
-        //     hide_team_identity: isTeamIdentity,
-        //     need_sme: "",
-        //     post_idea_privately: isIdeaPrivately,
-        //     terms_condiitons: isTermsConditons,
-        // }
-
-        // let arr = [];
-        // arr[0] = { key: "form_data", item: JSON.stringify(data_form) }
-        // arr[1] = { key: "form_data", item: JSON.stringify(data_form) }
-        // arr[2] = { key: "form_data", item: JSON.stringify(data_form) }
-        // arr[3] = { key: "form_data", item: JSON.stringify(data_form) }
-        // arr[4] = { key: "form_data", item: JSON.stringify(data_form) }
-
-
-        // }
-
-
     }
 
     const onItemList = () => {
@@ -226,11 +190,11 @@ function SubmitIdeaStep2(props) {
 
                     if (item.type == "text" || item.type == "ckeditor" || item.type == "textarea" || item.type == "tags")
                         return (
-                            <TextFieldItem title={item?.caption} required={item?.required} isKey={""} onCurrentText={(txt) => onTextChange(index, txt)} />
+                            <TextFieldItem title={item?.caption} required={item?.required} isKey={""} onCurrentText={(txt) => onTextChange(index, item.KeyType, txt)} />
                         );
                     else if (item.type == "number")
                         return (
-                            <TextFieldItem title={item?.caption} required={item?.required} isKey={"phone-pad"} onCurrentText={(txt) => onTextChange(index, txt)} />
+                            <TextFieldItem title={item?.caption} required={item?.required} isKey={"phone-pad"} onCurrentText={(txt) => onTextChange(index, item.KeyType, txt)} />
                         );
                     else if (item.type == "file")
                         return (
@@ -239,15 +203,15 @@ function SubmitIdeaStep2(props) {
                         );
                     else if (item.type == "multijson" && item.additional_images)
                         return (
-                            <BrowseEditionalImag title={item?.additional_images?.caption} required={item?.required} onMultiImageArr={(obj) => onSelectMultiImg(index, obj)} />
+                            <BrowseEditionalImag title={item?.additional_images?.caption} required={item?.required} onMultiImageArr={(obj) => onSelectMultiImg(index, item.KeyType, obj)} />
                         );
                     else if (item.type == "multijson")
                         return (
-                            <TeamMembersDetails data={item} required={item?.required} onSelectTeamMembers={(item) => onTeamMemeber(index, item)} />
+                            <TeamMembersDetails data={item} onSelectTeamMembers={(value) => onTeamMemeber(index, item.KeyType, value)} />
                         );
                     else if (item.type == "checkbox")
                         return (
-                            <TermAndConditions title={item?.caption} required={item?.required} onChecked={(value) => onTermConditions(index, value)} />
+                            <TermAndConditions title={item?.caption} required={item?.required} onChecked={(value) => onTermConditions(index, item.KeyType, value)} />
                         );
                 })}
             </View>
@@ -258,48 +222,6 @@ function SubmitIdeaStep2(props) {
         <View style={Style.MainView}>
 
             {isItemList.length > 0 && onItemList()}
-            {/*             
-            {isData?.idea_description && <TextFieldItem title={isData?.idea_description?.caption} required={isData?.idea_description?.required} isKey={""}
-                onCurrentText={(txt) => onTextChange("IdeaDescription", txt)} />}
-
-            {isData?.idea_cover_image && <BrowseFileItem title={isData?.idea_cover_image?.caption} required={isData?.idea_cover_image?.required}
-                type={"image"} size={isData?.idea_cover_image?.size?.size} onSelectImgResponse={onSelectImg} />}
-
-            {isData?.upload_additional_images && <BrowseEditionalImag title={isData?.upload_additional_images?.additional_images?.caption}
-                required={isData?.upload_additional_images?.required} onMultiImageArr={onSelectMultiImg} />}
-
-            {isData?.challenges_addressing && <TextFieldItem title={isData?.challenges_addressing?.caption} required={isData?.challenges_addressing?.required} isKey={""}
-                onCurrentText={(txt) => onTextChange("challengesAddressing", txt)} />}
-
-            {isData?.benefit_idea && <TextFieldItem title={isData?.benefit_idea?.caption} required={isData?.benefit_idea?.required} isKey={""}
-                onCurrentText={(txt) => onTextChange("benefitIdea", txt)} />}
-
-            {isData?.idea_upload_files && <BrowseFileItem title={isData?.idea_upload_files?.caption} required={isData?.idea_upload_files?.required}
-                type={"file"} size={isData?.idea_upload_files?.size?.size} onSelectImgResponse={onSelectFile} />}
-
-            {isData?.upload_video_url && <TextFieldItem title={isData?.upload_video_url?.caption} required={isData?.upload_video_url?.required} isKey={""}
-                onCurrentText={(txt) => onTextChange("videoUrl", txt)} />}
-
-            {isData?.upload_embed_url && <TextFieldItem title={isData?.upload_embed_url?.caption} required={isData?.upload_embed_url?.required} isKey={""}
-                onCurrentText={(txt) => onTextChange("UploadEmbedUrl", txt)} />}
-
-            {isData?.idea_upload_videos && <BrowseFileItem title={isData?.idea_upload_videos?.caption} required={isData?.idea_upload_videos?.required}
-                type={"video"} size={isData?.idea_upload_videos?.size?.size} onSelectImgResponse={onSelectVideoFile} />}
-
-            {isData?.keywords_tags && <TextFieldItem title={isData?.keywords_tags?.caption} required={isData?.keywords_tags?.required} isKey={""}
-                onCurrentText={(txt) => onTextChange("keywordsTags", txt)} />}
-
-            {isData?.no_of_team_member && <TextFieldItem title={isData?.no_of_team_member?.caption} required={isData?.no_of_team_member?.required} isKey={"phone-pad"}
-                onCurrentText={(txt) => onTextChange("NoOfTeamMember", txt)} />}
-
-            {isData?.team_member_details && <TeamMembersDetails data={isData?.team_member_details} required={isData?.team_member_details?.required}
-                onSelectTeamMembers={onTeamMemeber} />}
-
-
-            {isData?.hide_team_identity && <TermAndConditions title={isData?.hide_team_identity?.caption} required={isData?.hide_team_identity?.required} onChecked={(value) => setTeamIdentity(value)} />}
-            {isData?.post_idea_privately && <TermAndConditions title={isData?.post_idea_privately?.caption} required={isData?.post_idea_privately?.required} onChecked={(value) => setIdeaPrivately(value)} />}
-            {isData?.terms_condiitons && <TermAndConditions title={isData?.terms_condiitons?.caption} isClick={true} required={isData?.terms_condiitons?.required} onChecked={(value) => setTermsConditons(value)} />}
- */}
 
             <TouchableOpacity style={[Style.btn, { backgroundColor: themeColor.buttonColor }]} onPress={() => onCheckField()}>
                 <Text style={Style.txtBtn}>{Label.Submit}</Text>
